d126fd5010d101ab3b4beee3001e9ba4f865b068
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-03 11:30:47 +0200

    Remove unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'messages/new'
-  get 'messages/create'
+  # get 'messages/new'
+  # get 'messages/create'
   resources :rooms do
     resources :messages
   end
