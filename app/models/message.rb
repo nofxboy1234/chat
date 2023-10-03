29e2d78857bd5193f9175bd4c692eae884bfeb90
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-03 15:55:33 +0200

    Use websocket to stream messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :room
+
+  # Mirror what we're doing in the turbo_stream template, but over websocket
+  after_create_commit -> { broadcast_append_to room }
 end
