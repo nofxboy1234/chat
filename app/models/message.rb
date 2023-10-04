f92842afe9b60b96427410b105ac848c07bcb8aa
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-04 10:56:05 +0200

    Replace individual lifecycle callbacks with broadcasts_to helper method

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,8 @@ class Message < ApplicationRecord
   belongs_to :room
 
   # Mirror what we're doing in the turbo_stream template, but over websocket
-  after_create_commit -> { broadcast_append_to room, target: 'messages2' }
-  after_destroy_commit -> { broadcast_remove_to room, target: 'messages2' }
-  after_update_commit -> { broadcast_replace_to room, target: 'messages2' }
+  broadcasts_to :room, target: 'messages2'
+  # after_create_commit -> { broadcast_append_to room, target: 'messages2' }
+  # after_destroy_commit -> { broadcast_remove_to room, target: 'messages2' }
+  # after_update_commit -> { broadcast_replace_to room, target: 'messages2' }
 end
