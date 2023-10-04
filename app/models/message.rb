3c55c724a724c1832ea9b63133bdcbd1776cdf92
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-04 10:09:33 +0200

    Add after_destroy_commit callback on Message model

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,5 @@ class Message < ApplicationRecord
 
   # Mirror what we're doing in the turbo_stream template, but over websocket
   after_create_commit -> { broadcast_append_to room }
+  after_destroy_commit -> { broadcast_remove_to room }
 end
