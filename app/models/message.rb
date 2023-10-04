ff57dec1e1cbb3fd0358475e005bccc45118b24e
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-04 12:08:19 +0200

    Print from :after_create_commit callback to test it is being called from terminal

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,7 +3,10 @@ class Message < ApplicationRecord
 
   # Mirror what we're doing in the turbo_stream template, but over websocket
   # broadcasts_to :room
-  after_create_commit -> { broadcast_append_to room }
+  after_create_commit lambda {
+                        puts '************* after_create_commit ************'
+                        broadcast_append_to room
+                      }
   after_destroy_commit -> { broadcast_remove_to room }
   after_update_commit -> { broadcast_replace_to room }
 end
