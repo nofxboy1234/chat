0a1d432c80e0df843d9db9f18962b808407e343c
Dylan Palmboom <nofxboy1234@gmail.com>
2023-10-04 11:48:49 +0200

    Update broadcast method calls so that Room name change is broadcast

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,8 +2,8 @@ class Message < ApplicationRecord
   belongs_to :room
 
   # Mirror what we're doing in the turbo_stream template, but over websocket
-  # broadcasts_to :room, target: 'messages2'
-  after_create_commit -> { broadcast_append_to room, :messages2, target: 'messages2' }
-  after_destroy_commit -> { broadcast_remove_to room, :messages2, target: 'messages2' }
-  after_update_commit -> { broadcast_replace_to room, :messages2, target: 'messages2' }
+  # broadcasts_to :room
+  after_create_commit -> { broadcast_append_to room }
+  after_destroy_commit -> { broadcast_remove_to room }
+  after_update_commit -> { broadcast_replace_to room }
 end
